fa8fd713087611f742fd4c8378cfa18a4b9a6d3a
JCervantesB <65304872+JCervantesB@users.noreply.github.com>
2021-07-30 09:35:43 -0600

    Add files via upload

---
+++ taller2/src/js/script.js
@@ -0,0 +1,48 @@
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajeDelPrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDelPrecioConDescuento)/ 100;
+    return precioConDescuento;
+  
+  }
+  
+  function onClickButtonPriceDiscount() {
+    
+      const totalPrice = document.getElementById("totalPrice");
+      const priceValue = totalPrice.textContent;
+      
+      const inputCoupon = document.getElementById("inputCoupon");
+      const couponValue = inputCoupon.value;
+  
+      const resultCoupon = document.getElementById("priceWithCoupon");
+      const totalDiscount = document.getElementById("totalDiscount");
+      let descuento;
+      let precioConDescuento; 
+    
+      switch(true) {        
+        case couponValue === "JuanDC_es_Batman":
+          descuento = 15;
+          precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+          resultCoupon.innerText =`Cupón aplicado`;
+        break;
+        case couponValue === "pero_no_le_digas_a_nadie":
+          descuento = 30;
+          precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+          resultCoupon.innerText =`Cupón aplicado`;
+        break;
+        case couponValue === "es_un_secreto":
+          descuento = 25;
+          precioConDescuento = calcularPrecioConDescuento(priceValue,descuento);
+          resultCoupon.innerText =`Cupón aplicado`;
+        break;
+        default:
+          resultCoupon.innerText = "el cupón ingresado no es valido";
+      }
+      const summary = document.getElementById('summary');
+      const classtotalPrice = document.getElementById('totalPrice');
+      const newPrice = document.getElementById('newPrice');
+      if (couponValue === 'JuanDC_es_Batman' || couponValue === 'pero_no_le_digas_a_nadie' || couponValue === "es_un_secreto") {
+        summary.classList.add('newcolor');
+        classtotalPrice.classList.add('addline');
+        newPrice.innerText = `\n $${precioConDescuento}`;
+      }
+  }
